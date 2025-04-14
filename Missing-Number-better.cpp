726fd809c9e556df2bbe7dd2c731f48eead2d468
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-04-14 17:49:18 +0000

    [268][Time Beats: 21.48%][Memory Beats: 5.77%] - LeeetPush

---
+++ Missing-Number-better.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        int missingNumber(vector<int>& nums) {
+            //better approach using approach
+            int n=nums.size();
+            vector<int>hash(n+1,0);
+            for (int i=0;i<n;i++){
+                if (nums[i] >= 0 && nums[i] <= n) {
+                    hash[nums[i]] = 1;
+                }
+            }
+            for (int i=0;i<=n;i++){
+                if (hash[i]==0){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    };
