d60f3633cfa32b6ed53e26662fda2e6fcf5d5963
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-04-17 22:37:52 +0530

    [560] [Time Beats: 13.80%] [Memory Beats: 86.12%] - LeetPush

--- Subarray-Sum-Equals-K.cpp
+++ Subarray-Sum-Equals-K.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        //brute force
+        //better force
         int n=nums.size();
         int cnt=0;
-        for (int i=0;i<n;i++){
-            int sumarr=0;
-            for (int j=i;j<n;j++){
-                sumarr+=nums[j];
-                if (sumarr==k){
-                    cnt++;
-                }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j]; // running sum
+                if (sum == k) cnt++;
             }
         }
         return cnt;
