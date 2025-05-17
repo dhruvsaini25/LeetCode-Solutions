ed87068f1e164fb4d6bc666ff52ae6f5cb002312
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-05-17 17:16:38 +0000

    [34] [Time Beats: 100.00%] [Memory Beats: 52.53%] - LeeetPush

---
+++ Find-First-and-Last-Position-of-Element-in-Sorted-Array-optimal.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        //optimal approach via BINARY SEARCH
+        //first occurence
+        int low=0, high=n-1;
+        int first=-1;
+        while (low<=high){
+            int mid=(low+high)/2;
+            if (nums[mid]==target){
+                first=mid;
+                high=mid-1;
+            } else if(target<nums[mid]){
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        //last occurence
+        low=0, high=n-1;
+        int last=-1;
+        while (low<=high){
+            int mid=(low+high)/2;
+            if (nums[mid]==target){
+                last=mid;
+                low=mid+1;
+            } else if(target<nums[mid]){
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        
+        return {first, last};
+    }
+};
