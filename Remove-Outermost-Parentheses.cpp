c4b7bc8772399e97564e33998ac35ced3e0eacd7
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-06-14 22:42:34 +0530

    [1021] [Time Beats: 100.00%] [Memory Beats: 96.58%] - LeetPush

---
+++ Remove-Outermost-Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        //brute force
+        string result;
+        int depth=0;
+        for (char ch:s){
+            if (ch=='('){
+                if (depth>0) result+=ch;
+                depth++;
+            } else {
+                depth--;
+                if (depth>0) result+=ch;
+            }
+        }
+        return result;
+    }
+};
