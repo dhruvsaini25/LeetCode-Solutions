9f7ae645ad9eb95adebdc474795874c72ddbac37
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-06-25 23:15:46 +0530

    [8] [Time Beats: 7.12%] [Memory Beats: 56.40%] - LeetPush

---
+++ String-to-Integer-(atoi).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        //brute force
+        int i=0;
+        int n=s.length();
+        while (i<n && s[i]==' ') i++;
+        int sign=1;
+        if (i<n && (s[i]=='+' || s[i]=='-')){
+            sign =(s[i]=='-') ? -1:1;
+            i++;
+        }
+        long long result=0;
+        while (i<n && isdigit(s[i])){
+            result=result*10+(s[i]-'0');
+            if (sign==1 && result>INT_MAX) return INT_MAX;
+            if (sign==-1 && -result<INT_MIN) return INT_MIN;
+            i++;
+        }
+        return result*sign;
+    }
+};
